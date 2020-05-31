4195ce5bfa06eb8b36e4b1de89382686bb4cc7b2
吴敬 <wujing@fxiaoke.com>
2020-05-31 18:48:42 +0800

    按钮封装

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -1,5 +1,14 @@
 <script>
+import {
+    QBtn
+} from 'quasar'
 export default {
-    
+    extends: QBtn,
+    methods: {
+        click() {
+            alert('扩展一下');
+            QBtn.methods.click.apply(this);
+        }
+    }
 }
 </script>
--- src/pages/Package.vue
+++ src/pages/Package.vue
@@ -1,16 +1,24 @@
 <template>
     <div style="padding: 20px;">
-        <q-btn color="white" text-color="black" label="列设置"></q-btn>
+        <div>
+            <q-btn color="white" text-color="black" label="列设置"></q-btn>
+        </div>
+        <br>
+        <div>
+            <cus-btn color="white" text-color="black" label="列设置"></cus-btn>
+        </div>
     </div>
 </template>
 <script>
 import {
     QBtn
 } from 'quasar'
+import CusBtn from '../components/button'
 export default {
     name: 'PagePackage', //因为是基础组件
     components: {
-        QBtn
+        QBtn,
+        CusBtn
     }
 }
 </script>
