a6d341e1f791d8bdbc06b25ffaa5dfc39922ce40
吴敬 <wujing@fxiaoke.com>
2020-05-31 18:39:37 +0800

    新增主体

---
+++ src/pages/Package.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="padding: 20px;">
+        <q-btn color="white" text-color="black" label="列设置"></q-btn>
+    </div>
+</template>
+<script>
+import {
+    QBtn
+} from 'quasar'
+export default {
+    name: 'PagePackage', //因为是基础组件
+    components: {
+        QBtn
+    }
+}
+</script>
+
